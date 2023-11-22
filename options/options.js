965e3a42624cd2ddfec686e578660adaf3094f2b
Crowdbotics <team@crowdbotics.com>
2023-11-22 13:23:01 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "yway-games-44719",
+  url: "https://yway-games-44719.botics.co",
+  api: "https://yway-games-44719.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
